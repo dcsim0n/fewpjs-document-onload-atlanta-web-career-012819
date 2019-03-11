7606fff5473ac2e7c63d8632dd18b3a039588751
Dana Simmons <dana@simmons-audio.com>
2019-03-11 16:35:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+function setPId() {
+    let p = document.querySelector("#text")
+    p.innerHTML = "This is really cool!"
+}
+
+document.addEventListener("DOMContentLoaded", setPId)
